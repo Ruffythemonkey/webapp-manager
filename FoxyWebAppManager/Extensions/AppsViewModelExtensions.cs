77cb30a1119db2471723a0da4bfd817b2cae8347
Burn Pc <seraphim@escsoft.de>
2025-10-22 14:34:37 +0200

    Icon load bug fixed

---
+++ FoxyWebAppManager/Extensions/AppsViewModelExtensions.cs
@@ -0,0 +1,20 @@
+﻿using FoxyWebAppManager.ViewModels;
+using static FoxyWebAppManager.Helpers.FireFoxIniParser;
+
+namespace FoxyWebAppManager.Extensions
+{
+    public static class AppsViewModelExtensions
+    {
+        extension(AppsViewModel vm)
+        {
+            public void ProfilesWithWebApps()
+            { 
+                vm.FoxProfiles = IniReaderFireFox.LoadProfilesFromInstalledFF()
+               .Where(x => x.GetMainFolder().GetJson().taskbarTabs.IsAnyTaskBarTabItemInStartMenu())
+               .ToList() 
+               is { Count: > 0} apps ? apps : vm.FoxProfiles;
+            }
+     
+        }
+    }
+}
