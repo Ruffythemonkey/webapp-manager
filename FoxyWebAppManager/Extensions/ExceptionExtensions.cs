308bced4b985e7ddd612a6508db8d01085ca517d
Burn Pc <seraphim@escsoft.de>
2025-11-05 10:15:14 +0100

    exception exstension added

---
+++ FoxyWebAppManager/Extensions/ExceptionExtensions.cs
@@ -0,0 +1,16 @@
+﻿using FoxyWebAppManager.ContentDialogs;
+
+namespace FoxyWebAppManager.Extensions
+{
+    public static class ExceptionExtensions
+    {
+        extension(Exception exception)
+        {
+            public async Task ShowMessageUI()
+            {
+                var MessageDialog = new DialogException() { Message = exception.Message};
+                await MessageDialog.ContentDialog.ShowAsync();
+            }
+        }
+    }
+}
